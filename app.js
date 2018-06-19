c0e6eab02aea841ac318012b4504b7fc8079fddc
Ren <rennyandres@gmail.com>
2018-06-19 11:40:10 +0000

    Project set-up and functioning

---
+++ app.js
@@ -0,0 +1,32 @@
+// app.js
+
+// IMPORTS --------------------------------------------------------------------
+var express = require('express');
+var app = express();
+
+
+// CONFIG ---------------------------------------------------------------------
+app.set('view engine', 'ejs');
+app.use(express.static(process.cwd() + '/public'));
+
+
+// ROUTES ---------------------------------------------------------------------
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/api/whoami', function(req, res) {
+    res.json({
+        "ipaddress" : req.headers["x-forwarded-for"],
+        "language" : req.headers["accept-language"],
+        "software" : req.headers["user-agent"]
+    });
+});
+
+
+// LISTENING ------------------------------------------------------------------
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log('Serving on port ' + process.env.PORT);
+});
+
+
